909f35c0da692ad027be9f14bda555f86b8fe1da
Jose Eduardo Meza Canul <Lalo@192.168.1.131>
2024-12-22 22:12:12 -0600

    Minor fixes

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* <title>Merivilla Co.</title> */}
+        <title>Merivilla Co.</title>
       </Head>
-      <body style={{background: "#ffffff"}}>
+      <body>
         <Main />
         <NextScript />
       </body>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
 import ChatInterface from "@/components/ChatInterface";
-import { Heading } from "@chakra-ui/react";
 
 export default function Index() {
   return (
